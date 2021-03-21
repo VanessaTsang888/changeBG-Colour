3fa9c8816cc08ae62ef5904215a878327f1fc9cc
VanessaTsang888 <vantaidesign8@gmail.com>
2021-03-21 15:54:33 +0000

    Written comments to the App.tsx, the test file and deleted some code fromt the App.css file.

--- src/All Comments/App.tsx
+++ src/All Comments/App.tsx
@@ -8,16 +8,17 @@ function App() {
 
   // The color state is a variable (ourColor) monitored by React. When it changes, it will get rendered to the UI.
   // The elements affected by this, which will be the style of the Header Element ( <header> )
-  // The data inside the variable gets stored in the Component useState (this is part of the React library) which is a Component state.
+  // The data inside the variable gets stored in the useState (this is part of the React library) which is a Component state.
   const [ourColor, setOurColor] = useState<string>('#ce1126');
 
-  // useState = React Hook. 2 variables persists at render. setOurInputValue = function that update the value. Every time I call useState fn will
-  // return an array of 2 variables.
-  // The input box will have what ever I pass into useState, in this example is empty string.
+  // The input box state is a variable (ourInputValue) monitored by React. When it changes, it will rerender to the UI. 
+  // The elements affected by this will be the value of the Input Element ( <input> ).
   // As the user types in more keystrokes, this get updates via the setOurInputValue variable.  The input value variable (ourInputValue) stores
   // what the user has typed into the input box. 
-  // The data inside the ourInputValue variable gets stored in the useState which is the Component state.
-
+  // The data inside the ourInputValue variable gets stored in the useState which is a Component state.
+  // useState = React Hook. 2 variables persists at render. setOurInputValue = function that update the value. Every time I call useState fn will
+  // return an array of 2 variables.
+  // The input box will have what ever I pass into useState, this example is empty string.
   const [ourInputValue, setOurInputValue] = useState<string>('');
 
   // The return() statement is used to display or render the content to the UI. In the below example, h2, input box and btn gets displayed to the UI.
@@ -37,13 +38,9 @@ function App() {
         via the setOurInputValue variable  */} 
         <input type="text" value={ourInputValue} onChange={(event) => {setOurInputValue(event.target.value)}} />
 
-        {/* 
-        
-        Setting the setOurColor to the same value as the value in ourInputValue variable.
-        when user clicks the btn then the final value gets stored in the
-       ourInputValue variable in the Component state. Then the bg colour gets updated as React now knows that the bg style has changed.
-        
-        When user inputs their keystrokes, the ourInputValue variable gets updated. */}
+        {/* When user inputs their keystrokes, the ourInputValue variable gets updated. when user clicks the btn then the final value gets stored in the
+        ourInputValue variable in the Component state. Then the bg colour gets updated as React now knows that the bg style has changed. Setting the
+        setOurColor to the same value as ourInputValue */}
         <button onClick={() => (setOurColor(ourInputValue))}> Change Background Colour! </button>
 
       </header>
--- src/__tests__/App.spec.tsx
+++ src/__tests__/App.spec.tsx
@@ -2,6 +2,9 @@ import { render, screen } from '@testing-library/react';
 import React from 'react';
 import App from '../App';
 
+// Test Driven Development: process of elying on software requirements being converted to test cases before
+// software is fully developed and tracking all software dev by repeatedly testing the sofware against all test cases.
+
 // This is a simple jest test, for human readability we use the method signature "test(" .. 
 // .. as part of the readable statement. 
 // Example: "Test if h2 element contains the name of my app"
@@ -13,5 +16,5 @@ test("if h2 element contains the name of my App", () => {
     const ourH2 = screen.getByTestId('Title')
 
     // We Expect the html within our H2 Element to read "Use this Input Box to change background colour to your fav colour"
-    expect(ourH2.innerHTML).toBe("Use this Input Box to change background colour to your fav colour");
+    expect(ourH2.innerHTML).toBe("Use Input Box to change background colour");
 })
